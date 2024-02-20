5f009b9735fed31d604999c32cdbcde4c77c3eb1
Diego Choli <diegocholi@gmail.com>
2024-02-20 20:00:25 -0300

    MAUI v1 perform to analyze

---
+++ applicationMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace applicationMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ applicationMaui/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace applicationMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadPostsAsync();
+        }
+
+        private async void LoadPostsAsync()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+            var posts = JsonSerializer.Deserialize<List<Post>>(response);
+            postsListView.ItemsSource = posts;
+        }
+    }
+
+    public class Post
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+        [JsonPropertyName("body")]
+        public string? Body { get; set; }
+    }
+}
